0c6548e941e996ef1f8ee086921bfb48560593f7
Georgi Georgiev <georgi.georgiev@flatrocktech.com>
2015-03-23 12:34:09 +0200

    More relevant Factory method pattern example

--- factory_method.py
+++ factory_method.py
@@ -1,45 +1,136 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""http://ginstrom.com/scribbles/2007/10/08/design-patterns-python-style/"""
+"""
+http://en.wikipedia.org/wiki/Factory_method_pattern
+The good point over the SimpleFactory is you can subclass it to implement different ways to create objects
+For simple case, this abstract class could be just an interface
+This pattern is a "real" Design Pattern because it achieves the "Dependency Inversion Principle"
+a.k.a the "D" in S.O.L.I.D principles.
+"""
+
+from abc import ABCMeta, abstractmethod  # Be able writing abstract classes
 
 
-class GreekGetter:
+# Factories
 
-    """A simple localizer a la gettext"""
+class VideoFactory:  # This is an interface to keep it simple
+    VIDEO_TYPE_COMMERCIAL = 'commercial'
+    VIDEO_TYPE_TUTORIAL = 'tutorial'
 
     def __init__(self):
-        self.trans = dict(dog="σκύλος", cat="γάτα")
+        pass
 
-    def get(self, msgid):
-        """We'll punt if we don't have a translation"""
-        try:
-            return self.trans[msgid]
-        except KeyError:
-            return str(msgid)
+    def create_video(self, video_type, video_src):
+        raise NotImplementedError
 
 
-class EnglishGetter:
+class SiteOneVideoFactory(VideoFactory):
+    """
+    This site hosts its videos on Youtube
+    """
 
-    """Simply echoes the msg ids"""
+    def create_video(self, video_type, video_id):
+        if video_type == VideoFactory.VIDEO_TYPE_COMMERCIAL:
+            return YouTubeCommercialVideo(video_id)
 
-    def get(self, msgid):
-        return str(msgid)
+        elif video_type == VideoFactory.VIDEO_TYPE_TUTORIAL:
+            return YouTubeTutorialVideo(video_id)
+
+        else:
+            raise ValueError('Invalid video type {}'.format(video_type))
 
 
-def get_localizer(language="English"):
-    """The factory method"""
-    languages = dict(English=EnglishGetter, Greek=GreekGetter)
-    return languages[language]()
+class SiteTwoVideoFactory(VideoFactory):
+    """
+    This site hosts its videos on Vimeo
+    """
+    def create_video(self, video_type, video_id):
+        if video_type == VideoFactory.VIDEO_TYPE_COMMERCIAL:
+            return VimeoCommercialVideo(video_id)
 
-# Create our localizers
-e, g = get_localizer(language="English"), get_localizer(language="Greek")
-# Localize some text
-for msgid in "dog parrot cat bear".split():
-    print(e.get(msgid), g.get(msgid))
+        elif video_type == VideoFactory.VIDEO_TYPE_TUTORIAL:
+            return VimeoTutorialVideo(video_id)
+
+        else:
+            raise ValueError('Invalid video type {}'.format(video_type))
+
+
+# Entities
+
+class AbstractVideo:
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def get_video_data(self):
+        pass
+
+
+class YouTubeCommercialVideo(AbstractVideo):
+    def __init__(self, video_id, *args, **kwargs):
+        self._video_src = 'http://www.youtube.com/?watch={}'.format(video_id)
+
+        super(YouTubeCommercialVideo, self).__init__(*args, **kwargs)
+
+    def get_video_data(self):
+        return {'video_type': 'commercial', 'provider': 'Youtube', 'src': self._video_src}
+
+
+class YouTubeTutorialVideo(AbstractVideo):
+    def __init__(self, video_id, *args, **kwargs):
+        self._video_src = 'http://www.youtube.com/?watch={}'.format(video_id)
+
+        super(YouTubeTutorialVideo, self).__init__(*args, **kwargs)
+
+    def get_video_data(self):
+        return {'video_type': 'tutorial', 'provider': 'Youtube', 'src': self._video_src}
+
+
+class VimeoCommercialVideo(AbstractVideo):
+    def __init__(self, video_id, *args, **kwargs):
+        self._video_src = 'http://www.vimeo.com/?watch={}'.format(video_id)
+
+        super(VimeoCommercialVideo, self).__init__(*args, **kwargs)
+
+    def get_video_data(self):
+        return {'video_type': 'commercial', 'provider': 'Vimeo', 'src': self._video_src}
+
+
+class VimeoTutorialVideo(AbstractVideo):
+    def __init__(self, video_id, *args, **kwargs):
+        self._video_src = 'http://www.vimeo.com/?watch={}'.format(video_id)
+
+        super(VimeoTutorialVideo, self).__init__(*args, **kwargs)
+
+    def get_video_data(self):
+        return {'video_type': 'tutorial', 'provider': 'Vimeo', 'src': self._video_src}
+
+
+# Create videos for site one and site two
+if __name__ == "__main__":
+    site_one_factory = SiteOneVideoFactory()
+    site_two_factory = SiteTwoVideoFactory()
+
+    # SiteOne
+    commercial_video = site_one_factory.create_video(VideoFactory.VIDEO_TYPE_COMMERCIAL, '123commercialid')
+    tutorial_video = site_one_factory.create_video(VideoFactory.VIDEO_TYPE_TUTORIAL, '123tutorialid')
+
+    print 'SiteOne videos:'
+    print commercial_video.get_video_data(), tutorial_video.get_video_data()
+
+    # SiteTwo
+    commercial_video = site_two_factory.create_video(VideoFactory.VIDEO_TYPE_COMMERCIAL, '123commercialid')
+    tutorial_video = site_two_factory.create_video(VideoFactory.VIDEO_TYPE_TUTORIAL, '123tutorialid')
+
+    print 'SiteTwo videos:'
+    print commercial_video.get_video_data(), tutorial_video.get_video_data()
+
 
 ### OUTPUT ###
-# dog σκύλος
-# parrot parrot
-# cat γάτα
-# bear bear
+# SiteOne videos:
+# {'src': 'http://www.youtube.com/?watch=123commercialid', 'video_type': 'commercial', 'provider': 'Youtube'} {'src': 'http://www.youtube.com/?watch=123tutorialid', 'video_type': 'tutorial', 'provider': 'Youtube'}
+# SiteTwo videos:
+# {'src': 'http://www.vimeo.com/?watch=123commercialid', 'video_type': 'commercial', 'provider': 'Vimeo'} {'src': 'http://www.vimeo.com/?watch=123tutorialid', 'video_type': 'tutorial', 'provider': 'Vimeo'}
